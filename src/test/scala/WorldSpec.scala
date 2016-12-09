367a932eaf479f43d322075540e60be16c257435
Randhier <randhier@gmail.com>
2016-12-08 19:49:39 -0500

    More unit test for word

--- src/test/scala/WorldSpec.scala
+++ src/test/scala/WorldSpec.scala
@@ -4,16 +4,16 @@ import org.scalatest.{ FlatSpec, Matchers }
 
 class WorldSpec extends FlatSpec with Matchers {
   behavior of "World"
-  it should "Return Mercury" in {
+  it should "Return Mercury and id 1" in {
     def world = new World(1, "Mercury")
-    val name = world.name
-    name should be ("Mercury")
+    world.name should be ("Mercury")
+    world.id should be (1)
   }
 
-  it should "Return Venus" in {
-    def world = new World(1, "Venus")
-    val name = world.name
-    name should be("Venus")
+  it should "Return Venus and id 2" in {
+    def world = new World(2, "Venus")
+    world.name should be("Venus")
+    world.id should be (2)
   }
 }
 
